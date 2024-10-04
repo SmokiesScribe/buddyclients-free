4ab514798e7f0f776dc1c90f68be38015d8b959e
SmokiesScribe <141648408+SmokiesScribe@users.noreply.github.com>
2024-10-04 16:58:16 -0400

    sanitize popup content

--- includes/helpers/popup.php
+++ includes/helpers/popup.php
@@ -1,4 +1,5 @@
 <?php
+use BuddyClients\Includes\Popup;
 /**
  * Get help doc content.
  * 
@@ -9,12 +10,12 @@
 function bc_get_popup_content() {
     
     // Get post ID from ajax
-    $post_id = $_POST['postId'];
-    $url = $_POST['url'];
-    $raw_content = $_POST['rawContent'];
+    $post_id = isset( $_POST['postId'] ) ? intval( wp_unslash( $_POST['postId'] ) ) : null;
+    $url = isset( $_POST['url'] ) ? esc_url_raw( wp_unslash( $_POST['url'] ) ) : null;
+    $raw_content = isset( $_POST['rawContent'] ) ? sanitize_text_field( wp_unslash( $_POST['rawContent'] ) ) : null;
     
     // Format post content for popup
-    $content = BuddyClients\Includes\Popup::format_content( $post_id, $url, $raw_content );
+    $content = Popup::format_content( $post_id, $url, $raw_content );
     
     // Return formatted content
     echo $content;
