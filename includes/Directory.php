a07151fc25cad07f1f23645eaa26dd91388e15b1
SmokiesScribe <141648408+SmokiesScribe@users.noreply.github.com>
2024-10-04 13:43:26 -0400

    updated Directory class with new methods

--- includes/Directory.php
+++ includes/Directory.php
@@ -1,6 +1,8 @@
 <?php
 namespace BuddyClients\Includes;
 
+use WP_Filesystem;
+
 /**
  * Handles a single directory.
  * 
@@ -80,12 +82,69 @@ class Directory {
         if ( ! file_exists( $this->full_path ) ) {
             
             // Create the directory recursively with full permissions
-            mkdir( $this->full_path, 0755, true );
+            $this->create_dir_recur( $this->full_path );
             
             // Create an index file inside the directory
-            file_put_contents( $this->full_path . '/index.php', '<?php // Silence is golden.' );
+            $this->create_file( $this->full_path, '/index.php', '<?php // Silence is golden.' );
         }
         return $this;
+    }
+
+    /**
+     * Creates directory recursively.
+     * 
+     * @since 1.0.4
+     * 
+     * @param   string  $full_path  The full path to the directory.
+     */
+    private function create_dir_recur( $full_path ) {
+        global $wp_filesystem;
+        WP_Filesystem(); // Initialize the WP_Filesystem class
+
+        if ( empty( $full_path ) ) {
+            return;
+        }
+        
+        // Check if the directory already exists
+        if ( ! $wp_filesystem->exists( $full_path ) ) {
+            
+            // Get the parent directory
+            $parent_dir = dirname( $full_path );
+        
+            // Create the parent directory if it does not exist
+            if ( ! $wp_filesystem->exists( $parent_dir ) ) {
+                $wp_filesystem->mkdir( $parent_dir, 0755 );
+            }
+            
+            // Now create the target directory
+            $wp_filesystem->mkdir( $full_path, 0755 );
+        }        
+    }
+
+    /**
+     * Creates a file and updates its contents.
+     * 
+     * @since 1.0.4
+     * 
+     * @param   string  $dir_path   The full directory path.
+     * @param   string  $file_name  The full file name.
+     * @param   string  $contents   The contents of the file.
+     * 
+     * @return  bool    True on success, false on failure.
+     */
+    private function create_file( $dir_path, $file_name, $contents ) {
+        global $wp_filesystem;
+        WP_Filesystem();
+
+        // Use WP_Filesystem to write the file
+        $result = $wp_filesystem->put_contents( $dir_path . $file_name, $contents, FS_CHMOD_FILE );
+
+        // Check if the operation was successful
+        if ( false === $result ) {
+            return false;
+        } else {
+            return true;
+        }
     }
     
     /**
@@ -150,9 +209,9 @@ class Directory {
             $htaccess_content .= "</FilesMatch>\n\n";
             $htaccess_content .= "# Custom error page for 403 (Access Denied) error - redirect to homepage\n";
             $htaccess_content .= "ErrorDocument 403 " . site_url() . "/\n";
-    
-            // Create or update the .htaccess file with the specified content
-            file_put_contents($htaccess_file, $htaccess_content);
+
+            $file_dir = self::primary_dir();
+            $this->create_file( $file_dir, '/.htaccess', $htaccess_content );
             
             // Set the current site URL as the option value for future checks
             update_option('bc_htaccess_url', site_url());
