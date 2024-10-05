d800604a31a1b4801e29c962143f27d41f9dae9c
SmokiesScribe <141648408+SmokiesScribe@users.noreply.github.com>
2024-10-05 07:18:09 -0400

    sanitize values when creating plugin page

--- admin/helpers/pages.php
+++ admin/helpers/pages.php
@@ -7,13 +7,30 @@ use BuddyClients\Admin\PluginPage;
  * @since 0.1.0
  */
 function bc_create_plugin_page() {
-    
-    // Create page and return object
-    $plugin_page = ( new PluginPage( $_POST['args']['page_key'] ) )->create_page( $_POST['args'] );
+
+    // Check for args
+    if ( isset( $_POST['args']['page_key'], $_POST['args'] ) ) {
+        
+        // Sanitize each element in the 'args' array
+        $args_sanitized = array_map( 'sanitize_text_field', wp_unslash( $_POST['args'] ) );
+
+        // Unslash and sanitize 'page_key'
+        $page_key = sanitize_text_field( wp_unslash( $_POST['args']['page_key'] ) );
+        
+        // Proceed with PluginPage creation using sanitized inputs
+        $plugin_page = ( new PluginPage( $page_key ) )->create_page( $args_sanitized );
+    } else {
+        // Handle the case where 'args' or 'page_key' is not set
+        $error_message = __( 'Invalid input for creating new page.', 'buddyclients' );
+        echo wp_json_encode([ 
+            'success' => false, 
+            'error_message' => $error_message
+        ]);
+    }  
     
     // Check if page was created
     if ( $plugin_page->post_id ) {
-        echo json_encode( [
+        echo wp_json_encode( [
             'success' => true, 
             'edit_post_url' => $plugin_page->edit_post_url, 
             'permalink' => $plugin_page->permalink, 
@@ -24,7 +41,7 @@ function bc_create_plugin_page() {
     } else {
         // If page creation failed, return an error message
         $error_message = __( 'Failed to create a new page.', 'buddyclients' );
-        echo json_encode([ 
+        echo wp_json_encode([ 
             'success' => false, 
             'error_message' => $error_message
         ]);
